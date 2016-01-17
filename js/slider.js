3dfc5880abacc6c9880e05e9fdee240c4d037354
Aleksandr <Sanyok_94@bk.ru>
2016-01-17 16:32:01 +0300

    ver 0/0/2a

--- js/slider.js
+++ js/slider.js
@@ -1,22 +1,20 @@
-/**
- * Created by админ on 17.01.16.
- */
 jQuery(document).ready(function(){
+    var i = 1;
     sliderGo();
     function sliderGo(){
-        var i = 1;
-        var intervalId = setInterval(function() {
+        if(i = 7) i=1;
             $li(i).css("left", "-900px");
             $li(i).css("display", "inline-block");
-
             $li(i).animate({left: "0px"}, 1000);
             $li(i).animate({left: "0px"}, 1000);
             $li(i).animate({left: "900px"},{
                 duration: 1000,
-                complete: function(){jQuery(this).css("display", "none")}
+                complete: function(){
+                    jQuery(this).css("display", "none");
+                    ++i;
+                    sliderGo()
+                }
             });
-            if(++i > 7) i=1;
-        }, 2900);
     }
     function $li(numb) {
         if (numb) {
